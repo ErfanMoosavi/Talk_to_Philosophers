1f9926fb3dec876b913ad32d5c76afab9278ef31
Erfan Moosavi <erfanmoosavi84@gmail.com>
2025-10-02 13:54:40 +0330

    Renamed internal method

--- src/internals/chat_completer.py
+++ src/internals/chat_completer.py
@@ -8,13 +8,13 @@ class ChatCompleter:
     def __init__(self):
         self.client = OpenAI(base_url=BASE_URL, api_key=API_KEY)
 
-    def msg_to_dict(self, msg: Message) -> dict[str, str]:
-        return {"role": msg.role, "content": msg.content}
-
     def complete_chat(self, messages: list[Message]) -> str:
-        completion_messages = [self.msg_to_dict(msg) for msg in messages]
+        completion_messages = [self._msg_to_dict(msg) for msg in messages]
 
         completion = self.client.chat.completions.create(
             model=MODEL_NAME, messages=completion_messages
         )
         return completion.choices[0].message.content.strip()
+
+    def _msg_to_dict(self, msg: Message) -> dict[str, str]:
+        return {"role": msg.role, "content": msg.content}
